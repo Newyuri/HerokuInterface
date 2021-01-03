2fe189b319a3dec46168cde011a18c0d2ca905de
武国斌 <wuguobin@wuguobindeMacBook-Pro.local>
2021-01-03 13:46:30 +0800

    alter

--- q.php
+++ q.php
@@ -5,17 +5,55 @@ header('Content-Type:application/json; charset=utf-8');
 // $answer['config']=getenv('HAVESEX');
 // exit(json_encode($answer)); 
 
-$link = mysql_connect(getenv('DB_HOST'),getenv('DB_USER'),getenv('DB_PWD')); 
 
-if (!$link) {  
-	die('Could not connect to MySQL: ' . mysql_error()); 
+$mysql_conf = array(
+
+  'host'  => getenv('DB_HOST'),
+
+  'db'   => '',
+
+  'db_user' => getenv('DB_USER'),
+
+  'db_pwd' => getenv('DB_PWD')
+
+);
+
+$mysqli = @new mysqli($mysql_conf['host'], $mysql_conf['db_user'], $mysql_conf['db_pwd']);
+
+if ($mysqli->connect_errno) {
+
+  die("could not connect to the database:\n" . $mysqli->connect_error);//诊断连接错误
+
 } else {
-	echo 'Connection OK';
-	mysql_close($link); 
+	echo "connect right";
 }
 
+$mysqli->query("set names 'utf8';");//编码转化
 
+$select_db = $mysqli->select_db($mysql_conf['db']);
 
+if (!$select_db) {
 
-//echo HM;
+  die("could not connect to the db:\n" . $mysqli->error);
+
+}$sql = "select uid from user where name = 'joshua';";
+
+$res = $mysqli->query($sql);
+
+if (!$res) {
+
+  die("sql error:\n" . $mysqli->error);
+
+}
+
+ while ($row = $res->fetch_assoc()) {
+
+    var_dump($row);
+
+  }
+
+$res->free();
+
+$mysqli->close();
+
 ?>
