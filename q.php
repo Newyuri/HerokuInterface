90403f2c2010c0f996f293981fa81a0afa0d1059
武国斌 <wuguobin@wuguobindeMacBook-Pro.local>
2021-01-03 14:23:49 +0800

    alter

--- q.php
+++ q.php
@@ -14,10 +14,9 @@ $mysqli = @new mysqli($mysql_conf['host'], $mysql_conf['db_user'], $mysql_conf['
 
 if ($mysqli->connect_errno) {
 	die("could not connect to the database:\n" . $mysqli->connect_error);
-	$answer['error']=getenv('HAVESEX');
-
+	$answer['error'] = "connect error";
 } else {
-	$answer['config']=getenv('HAVESEX');
+	$answer['succeed'] = "connect succeed";
 }
 
 $mysqli->query("set names 'utf8';");//编码转化
@@ -25,9 +24,8 @@ $mysqli->query("set names 'utf8';");//编码转化
 $select_db = $mysqli->select_db($mysql_conf['db']);
 
 if (!$select_db) {
-
 	die("could not connect to the db:\n" . $mysqli->error);
-
+	$answer['error'] = "connect db error";
 }
 
 $sql = "select * from gettime where id = '0';";
@@ -35,16 +33,14 @@ $sql = "select * from gettime where id = '0';";
 $res = $mysqli->query($sql);
 
 if (!$res) {
-
   die("sql error:\n" . $mysqli->error);
-
+  $answer['error'] = "select error";
 }
 
 while ($row = $res->fetch_assoc()) {
-
   var_dump($row);
-  echo $row['title'];
-  echo $row['time'];
+  $answer['title'] = $row['title'];
+  $answer['time'] = $row['time'];
 }
 
 $res->free();
