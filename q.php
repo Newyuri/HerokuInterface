49c0bc4092b30279b9e52fbeb838113a6d53e86a
武国斌 <wuguobin@wuguobindeMacBook-Pro.local>
2021-01-03 13:31:36 +0800

    alter

--- q.php
+++ q.php
@@ -1,12 +1,21 @@
 <?php
 header('Content-Type:application/json; charset=utf-8');
 
-$conn = mysql_connect(getenv('DB_HOST'),getenv('DB_USER'),getenv('DB_PWD'));
-if ($conn) {
-	$answer = array();
-	$answer['config']=getenv('CONFIGKEY');
-	exit(json_encode($answer)); 
+// $answer = array();
+// $answer['config']=getenv('HAVESEX');
+// exit(json_encode($answer)); 
+
+$link = mysql_connect(getenv('DB_HOST'),getenv('DB_USER'),getenv('DB_PWD')); 
+
+if (!$link) {  
+	die('Could not connect to MySQL: ' . mysql_error()); 
+} else {
+	echo 'Connection OK';
+	mysql_close($link); 
 }
+
+
+
 
 //echo HM;
 ?>
