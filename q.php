d1e1461e887aba2f936732a38fccca142e208734
武国斌 <wuguobin@wuguobindeMacBook-Pro.local>
2021-01-03 13:18:21 +0800

    alter

--- q.php
+++ q.php
@@ -1,9 +1,12 @@
 <?php
 header('Content-Type:application/json; charset=utf-8');
 
-$answer = array();
-$answer['config']=getenv('CONFIGKEY');
-exit(json_encode($answer)); 
+$conn = mysql_connect(getenv('DB_HOST'),getenv('DB_USER'),getenv('DB_PWD'));
+if ($conn) {
+	$answer = array();
+	$answer['config']=getenv('CONFIGKEY');
+	exit(json_encode($answer)); 
+}
 
 //echo HM;
 ?>
