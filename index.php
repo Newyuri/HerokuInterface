d229effd8e07d7b7d3bc004164cb0333b1bf02eb
武国斌 <wuguobin@wuguobindeMacBook-Pro.local>
2021-01-03 14:28:21 +0800

    alter

--- index.php
+++ index.php
@@ -1,5 +1,51 @@
 <?php
 header('Content-Type:application/json; charset=utf-8');
-$arr = array('title'=>'出成绩','time'=>'2021-02-27');
-exit(json_encode($arr)); 
+
+$answer = array();
+
+$mysql_conf = array(
+  'host'  => getenv('DB_HOST'),
+  'db'   => getenv('DB_DB'),
+  'db_user' => getenv('DB_USER'),
+  'db_pwd' => getenv('DB_PWD')
+);
+
+$mysqli = @new mysqli($mysql_conf['host'], $mysql_conf['db_user'], $mysql_conf['db_pwd']);
+
+if ($mysqli->connect_errno) {
+	die("could not connect to the database:\n" . $mysqli->connect_error);
+	$answer['error'] = "connect error";
+} else {
+	$answer['succeed'] = "connect succeed";
+}
+
+$mysqli->query("set names 'utf8';");
+
+$select_db = $mysqli->select_db($mysql_conf['db']);
+
+if (!$select_db) {
+	die("could not connect to the db:\n" . $mysqli->error);
+	$answer['error'] = "connect db error";
+}
+
+$sql = "select * from gettime where id = '0';";
+
+$res = $mysqli->query($sql);
+
+if (!$res) {
+  die("sql error:\n" . $mysqli->error);
+  $answer['error'] = "select error";
+}
+
+while ($row = $res->fetch_assoc()) {
+  //var_dump($row);
+  $answer['title'] = $row['title'];
+  $answer['time'] = $row['time'];
+}
+
+$res->free();
+
+$mysqli->close();
+exit(json_encode($answer))
+
 ?>
